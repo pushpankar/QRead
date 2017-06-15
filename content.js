0c84f23e61d275d42970758113cb85e0129daec3
Pushpankar <pushpankark@gmail.com>
2017-06-15 12:11:44 +0530

    Embolden substring

--- content.js
+++ content.js
@@ -13,5 +13,16 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       value: clickedEl.value
     });
     console.log(clickedEl.innerHTML);
+    highlight(clickedEl, 10, 20);
   }
 });
+
+function highlight(inputText, start, end) {
+  // inputText = document.getElementById("inputText")
+  var innerHTML = inputText.innerHTML;
+  // var index = innerHTML.indexOf(text);
+  innerHTML = innerHTML.substring(0, start) + "<span style='font-weight: bold '>" + innerHTML.substring(start, end) + "</span>" + innerHTML.substring(end);
+  inputText.innerHTML = innerHTML;
+  console.log(innerHTML);
+  return innerHTML;
+};
